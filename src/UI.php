13597ee03d5a1379976e8bc1d45d8b76cb384f7a
Max Semenik <maxsem.wiki@gmail.com>
2016-09-13 16:49:58 -0700

    Show when data was updated

--- src/UI.php
+++ src/UI.php
@@ -2,6 +2,8 @@
 
 namespace OsmLint;
 
+use DateTime;
+use DateTimeZone;
 
 class UI {
 	const MAIN_HEADING = 'Problems connecting OSM and Wikipedia/Wikidata';
@@ -9,11 +11,13 @@ class UI {
 	private $request;
 	private $environment;
 	private $checker;
+	private $dataFile;
 
 	public function __construct( array $request, Environment $environment ) {
 		$this->request = $request;
 		$this->environment = $environment;
 		$this->checker = new Checker( $environment );
+		$this->dataFile = __DIR__ . '/../../problems.json';
 	}
 
 	public function show() {
@@ -134,7 +138,7 @@ HTML;
 	}
 
 	private function getData() {
-		$data = @file_get_contents( __DIR__ . '/../../problems.json' );
+		$data = @file_get_contents( $this->dataFile );
 		if ( $data === false ) {
 			$this->error( 500, 'Error reading data file' );
 		}
@@ -166,10 +170,49 @@ HTML;
 	}
 
 	private function showFooter() {
+		$time = stat( $this->dataFile )['mtime'];
+		$diff = self::timeElapsed( "@$time" );
 		echo <<<HTML
+Last updated $diff.
 <a href="https://github.com/MaxSem/osmlint"><img style="position: absolute; top: 0; right: 0; border: 0;" src="https://camo.githubusercontent.com/365986a132ccd6a44c23a9169022c0b5c890c387/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f6769746875622f726962626f6e732f666f726b6d655f72696768745f7265645f6161303030302e706e67" alt="Fork me on GitHub" data-canonical-src="https://s3.amazonaws.com/github/ribbons/forkme_right_red_aa0000.png"></a>
 <div id="bottom"><a href="https://tools.wmflabs.org/"><img src="https://tools-static.wmflabs.org/static/logos/powered-by-tool-labs.png" /></a></div>
 </body>
 HTML;
+	}
+
+	/**
+	 * From http://stackoverflow.com/a/18602474/1033612
+	 * @param int $datetime
+	 * @param bool $full
+	 * @return string
+	 */
+	private static function timeElapsed($datetime, $full = false) {
+		$utc = new DateTimeZone( 'UTC' );
+		$now = new DateTime( 'now', $utc );
+		$ago = new DateTime( $datetime, $utc );
+		$diff = $now->diff($ago);
+
+		$diff->w = floor($diff->d / 7);
+		$diff->d -= $diff->w * 7;
+
+		$string = array(
+			'y' => 'year',
+			'm' => 'month',
+			'w' => 'week',
+			'd' => 'day',
+			'h' => 'hour',
+			'i' => 'minute',
+			's' => 'second',
+		);
+		foreach ($string as $k => &$v) {
+			if ($diff->$k) {
+				$v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+			} else {
+				unset($string[$k]);
+			}
+		}
+
+		if (!$full) $string = array_slice($string, 0, 1);
+		return $string ? implode(', ', $string) . ' ago' : 'just now';
 	}
 }
